27324635daee7f21ccde860c2060be621c7acc38
Abdullah Azeem <abdullahazeem770@gmail.com>
2024-10-18 00:06:45 +0530

    created Account contract and AccountFactory contract

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # TO-Do
 1. Frontend Social media app for demo
 2. Our Main frontend (iske through companies hume contact krengi to show their ads and pay us, this money would go into the main account which funds the paymaster) (what percentage of the money should remain in our paymaster account and what percentage should go in EntryPoint.sol?)
-3. For first transaction of the user using paymaster set the initCode to 1
-4. Initcode contains the factory contract address
-5. 
+3. Initcode contains the factory contract address
+4. 
---
+++ src/ZapAccountFactory.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.23;
+
+import "./ZapAccount.sol";
+contract ZapAccountFactory{
+    IEntryPoint private immutable _entryPoint;
+    constructor(IEntryPoint anEntryPoint) {
+        _entryPoint = anEntryPoint;
+    }
+
+    function createAccount(address owner, uint256 salt) public returns (ZapAccount ret) {
+        address addr = getAddress(owner, salt);
+        uint256 codeSize = addr.code.length;
+        if (codeSize > 0) {
+            return ZapAccount(payable(addr));
+        }
+        ret = new ZapAccount{salt: bytes32(salt)}(IEntryPoint(owner));
+        return ret;
+    }
+
+    function getAddress(address owner, uint256 salt) public view returns (address) {
+        bytes32 byteSalt = bytes32(salt);
+        bytes memory bytecode = abi.encodePacked(
+            type(ZapAccount).creationCode,
+            abi.encode(owner)
+        );
+        bytes32 hash = keccak256(
+            abi.encodePacked(
+                bytes1(0xff),
+                address(this),
+                byteSalt,
+                keccak256(bytecode)
+            )
+        );
+        return address(uint160(uint256(hash)));
+    }
+}
