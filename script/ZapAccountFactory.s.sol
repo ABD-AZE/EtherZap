19ce58c9b07ace0eb5acef4ccccca2a8272f8c11
Abdullah Azeem <abdullahazeem770@gmail.com>
2024-10-19 14:34:24 +0530

    modified ZapAccountFactory.s.sol

--- script/ZapAccountFactory.s.sol
+++ script/ZapAccountFactory.s.sol
@@ -1,12 +1,13 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.23;
 
-import {Script} from "../lib/forge-std/src/Script.sol";
-import "../lib/account-abstraction/contracts/core/EntryPoint.sol";
+import "lib/forge-std/src/Script.sol";
+import "lib/account-abstraction/contracts/interfaces/IEntryPoint.sol";
 import "../src/ZapAccountFactory.sol";
 contract MyZapAccountFactory is Script{ 
-    IEntryPoint private immutable _entryPoint= IEntryPoint(0xEdf47C7E665bEb76b216205573935236f89ae83A);
+    IEntryPoint private i_entrypoint;
     function run() external {
+        i_entrypoint = IEntryPoint(0x0576a174D229E3cFA37253523E645A78A0C91B57);
         vm.startBroadcast();
         ZapAccountFactory factory = new ZapAccountFactory(_entryPoint);
         vm.stopBroadcast();
