0849f6e90bd70bb71f360ef263cc32d609798ee2
Abdullah Azeem <abdullahazeem770@gmail.com>
2024-10-19 11:21:03 +0530

    modified paymaster

---
+++ script/ZapAccountFactory.s.sol
@@ -0,0 +1,17 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.23;
+
+import "lib/forge-std/src/Script.sol";
+import "lib/account-abstraction/contracts/core/EntryPoint.sol";
+import "../src/ZapAccountFactory.sol";
+contract MyZapAccountFactory is Script{ 
+    EntryPoint private immutable i_entrypoint;
+    constructor(EntryPoint entrypoint) {
+        i_entrypoint=entrypoint;
+    }
+    function run() external {
+        vm.startBroadcast();
+        ZapAccountFactory factory = new ZapAccountFactory(i_entrypoint);
+        vm.stopBroadcast();
+    }
+}
