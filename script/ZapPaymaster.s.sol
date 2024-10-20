2739975a6f1c262156d54f5e8229ff1aeb88787c
Abdullah Azeem <abdullahazeem770@gmail.com>
2024-10-20 22:07:08 +0530

    ZapExecute.s.sol modified

---
+++ script/ZapPaymaster.s.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.8.9;
+
+import {Script} from "../lib/forge-std/src/Script.sol";
+import "../src/ZapPaymaster.sol";
+
+contract DeployPaymaster is Script{
+    address public EP = 0x0000000071727De22E5E9d8BAf0edAc6f37da032;
+    function run() external {
+        vm.startBroadcast();
+        ZapPayMaster zpm = new ZapPayMaster(EP);
+        vm.stopBroadcast();
+    }
+}
